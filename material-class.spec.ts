04eea2e42ef3b1f1d1dba377eb690c1996910eba
trigueur <thomasbenmezded@gmail.com>
2023-10-25 17:04:28 +0200

    front first commit

---
+++ material-class.spec.ts
@@ -0,0 +1,7 @@
+import { MaterialClass } from './material-class';
+
+describe('MaterialClass', () => {
+  it('should create an instance', () => {
+    expect(new MaterialClass()).toBeTruthy();
+  });
+});
---
+++ material-class.ts
@@ -0,0 +1,8 @@
+export class MaterialClass {
+
+    public id?: number;
+    public name?: string;
+    public material?: string;
+    public indexRecyclability?: string;
+    public isrecyclable?: string;
+}
